4faaadfe91368d74b0551d1c8f45e09abb9fc8dc
yousseffagla-droid <yousseffagla@gmail.com>
2026-09-19 15:29:05 +0300

    feat: add orchestrator core - index.js

---
+++ backend/orchestrator/index.js
@@ -0,0 +1,7 @@
+export * from './contracts.js';
+export * from './capability-registry.js';
+export * from './task-router.js';
+export * from './task-decomposer.js';
+export * from './result-aggregator.js';
+export * from './capabilities.js';
+export * from './orchestrator.js';
