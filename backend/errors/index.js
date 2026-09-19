dadd68a60f77c59cb2c521f69393e1adfe61dc28
yousseffagla-droid <yousseffagla@gmail.com>
2026-09-19 14:13:48 +0300

    feat: harden core domain and runtime

--- backend/errors/index.js
+++ backend/errors/index.js
@@ -1,9 +1,15 @@
-export class ToolExecutionError extends Error { constructor(message, options) { super(message, options); this.name = 'ToolExecutionError'; } }
-export class AuthenticationError extends Error { constructor(message, options) { super(message, options); this.name = 'AuthenticationError'; } }
-export class ValidationError extends Error { constructor(message, options) { super(message, options); this.name = 'ValidationError'; } }
-export class PermissionDeniedError extends Error { constructor(message, options) { super(message, options); this.name = 'PermissionDeniedError'; } }
-export class TimeoutError extends Error { constructor(message, options) { super(message, options); this.name = 'TimeoutError'; } }
-export class ExternalServiceError extends Error { constructor(message, options) { super(message, options); this.name = 'ExternalServiceError'; } }
-export class ModelError extends Error { constructor(message, options) { super(message, options); this.name = 'ModelError'; } }
-export class ApprovalRequiredError extends Error { constructor(message, options) { super(message, options); this.name = 'ApprovalRequiredError'; } }
-export class InvalidToolInvocationError extends Error { constructor(message, options) { super(message, options); this.name = 'InvalidToolInvocationError'; } }
+export class FaglaError extends Error {
+  constructor(message, options = {}) { super(message, options); this.name = new.target.name; this.code = options.code ?? this.name; }
+}
+export class AgentContractError extends FaglaError {}
+export class InvalidTaskTransition extends FaglaError {}
+export class InvalidToolInvocationError extends FaglaError {}
+export class PermissionDeniedError extends FaglaError {}
+export class ApprovalRequiredError extends FaglaError {}
+export class AgentExecutionError extends FaglaError {}
+export class ValidationError extends FaglaError {}
+export class ToolExecutionError extends FaglaError {}
+export class AuthenticationError extends FaglaError {}
+export class TimeoutError extends FaglaError {}
+export class ExternalServiceError extends FaglaError {}
+export class ModelError extends FaglaError {}
